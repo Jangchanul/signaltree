dd62fdc12f16f2ad8680e05b4f37a443a92f70d4
chanul <99154985+Jangchanul@users.noreply.github.com>
2023-07-20 20:26:08 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 $(function () {
+  $(".bg").click(function(){
+    $(".modal").hide();  
+    $("#menuToggle input:checkbox").prop("checked", false);
+  });
+  $(".bt_ok").click(function(){
+    $(".modal").hide();
+  });
   $(".dep1>li").hover(function () {
-    $(this).children().addClass("active");
+    $(this).toggleClass("active");
+    $(this).children().stop().toggleClass("active");
+  });
+  $(".tg_bt").click(function(){
+    $(this).toggleClass("active");
+    $(this).siblings().toggleClass("active");
+  });
+  $(".bt_guide").click(function(){
+    $(".modal").show();
+  });
+  $(".bg").click(function(){
+    $(".modal").hide();  
+    $("#menuToggle input:checkbox").prop("checked", false);
+  });
+  $(".bt_ok").click(function(){
+    $(".modal").hide();
+  });
+  $(".bt_close").click(function(){
+    $(".modal").hide();
+  });
+  $(".m_menu .mid>li div").click(function(){
+    $(this).toggleClass("active");
+    $(this).siblings().slideToggle();
+  });
+  $(".m_main .touch_line").click(function(){
+    // $(this).siblings().css('touch-action','pan-y');
+    $(this).siblings().slideToggle();
+  });
+  $(".agree1").click(function(){
+    $(".fade1").show();
   });
 });
-console.log("hi");
